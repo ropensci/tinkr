3eb6ab51c61e9f0a64f2177ef4877a8d884d3317
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-18 09:01:23 +0200

    oops again

--- tests/testthat/test-to_md.R
+++ tests/testthat/test-to_md.R
@@ -35,8 +35,8 @@ test_that("to_md works for Rmd", {
   to_md(yaml_xml_list, "newmd.Rmd")
   expect_true(file.exists("newmd.Rmd"))
   expect_silent(to_xml("newmd.Rmd"))
-  expect_true(stringr::str_detect("julia",
-                                  readLines("newmd.Rmd")))
+  expect_true(stringr::str_detect(readLines("newmd.Rmd"),
+                                  "julia"))
   file.remove("newmd.Rmd")
 })
 
