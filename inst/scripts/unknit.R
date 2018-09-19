9822316a96e28ab308ee7218766a3ae0564557fd
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-19 14:06:34 +0200

    unknitting experiments

---
+++ inst/scripts/unknit.R
@@ -0,0 +1,11 @@
+rmarkdown::render("inst/scripts/rmd_doc.Rmd")
+doc <- docxtractr::read_docx("inst/scripts/rmd_doc.docx")
+class(doc)
+rmarkdown::pandoc_convert(input = file.path(getwd(),
+                                            "inst/scripts/rmd_doc.docx"),
+                          to = "gfm",
+                          output = "rmd_doc_pandoc.Rmd")
+
+library("magrittr")
+tinkr::to_xml("inst/scripts/rmd_doc_pandoc.Rmd") %>%
+  tinkr::to_md("inst/scripts/rmd_doc_loop.Rmd")
