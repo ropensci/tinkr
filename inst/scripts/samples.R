8e22446a032022a3a443b1776e834c5b3f3e4658
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-19 14:06:06 +0200

    compare md

--- inst/scripts/samples.R
+++ inst/scripts/samples.R
@@ -1,3 +1,6 @@
 library("magrittr")
 tinkr::to_xml("inst/extdata/bigsample.md") %>%
   tinkr::to_md("inst/extdata/bigsample_after_loop.md")
+
+diffr::diffr(file1 = "inst/extdata/bigsample.md",
+             file2 = "inst/extdata/bigsample_after_loop.md")
