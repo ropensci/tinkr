b97ef02c0d65d39bf9b37c8ed0b64fd8e9a8f973
Zhian N. Kamvar <zkamvar@gmail.com>
2020-11-04 11:17:41 -0800

    update to_md() to output rendered document
     - stylesheet is now checked for validity
     - path is now an optional argument in `to_md()`
     - to_md() now returns the rendered markdown, invisibly
     - several parts of to_md() were split off into new functions

---
+++ R/stylesheet.R
@@ -0,0 +1,31 @@
+is_stylesheet <- function(stylesheet) {
+  inherits(stylesheet, "xml_document") && 
+    length(xml2::xml_name(stylesheet)) == 1L    &&
+    xml2::xml_name(stylesheet) == "stylesheet"
+}
+
+read_stylesheet <- function(stylesheet_path) {
+  
+  # if the stylesheet already is an XML stylesheet, just return.
+  if (is_stylesheet(stylesheet_path)) {
+    return(stylesheet_path)
+  }
+
+  null_or_na <- is.null(stylesheet_path) ||
+    length(stylesheet_path) != 1L ||
+    any(is.na(stylesheet_path))
+
+  if (null_or_na) {
+    stop("'stylesheet_path' must be a path to an XSL stylesheet", call. = FALSE)
+  }
+  if (!file.exists(stylesheet_path)) {
+    stop(glue::glue("The file '{stylesheet_path}' does not exist."), call. = FALSE)
+  }
+  out <- xml2::read_xml(stylesheet_path)
+  if (is_stylesheet(out)) {
+    return(out)
+  } else {
+    stop(glue::glue('{stylesheet_path} is not a valid stylesheet'))
+  }
+  
+}
