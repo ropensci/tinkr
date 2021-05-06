831c27f799f8e47a4be4971402adc54170423a59
Zhian N. Kamvar <zkamvar@gmail.com>
2021-05-05 17:33:06 -0700

    update inline math parsing
    Deer lord, I hope this makes sense six months down the line

--- R/asis-nodes.R
+++ R/asis-nodes.R
@@ -45,19 +45,80 @@ find_broken_math <- function(math) {
   )
 }
 
+#' Find and protect all inline math elements
+#' 
+#' @param body an XML document
+#' @param ns an XML namespace
+#' @return a modified _copy_ of the original XML document
+#' @keywords internal
+#' @examples
+#' txt <- commonmark::markdown_xml(
+#'   r"{This sentence contains $I_A$ $\frac{\pi}{2}$ inline $\LaTeX$ math.}"
+#' )
+#' txt <- xml2::read_xml(txt)
+#' cat(to_md(list(body = txt, yaml = "")), sep = "\n")
+#' ns  <- xml2::xml_ns_rename(xml2::xml_ns(txt), d1 = "md")
+#' protxt <- tinkr:::protect_inline_math(txt, ns)
+#' cat(to_md(list(body = protxt, yaml = "")), sep = "\n")
 protect_inline_math <- function(body, ns) {
-  math <- find_inline_math(body, ns)
+  body  <- copy_xml(body)
+  math  <- find_inline_math(body, ns)
   broke <- find_broken_math(math)
 
+  bespoke  <- !(broke$no_end | broke$no_beginning)
+  endless  <- broke$no_end[!bespoke]
+  headless <- broke$no_beginning[!bespoke]
+
+  imath   <- math[bespoke]
+  bmath   <- math[!bespoke]
+
   # protect math that is strictly inline
-  bespoke <- !(broke$no_end | broke$no_beginning)
-  new_nodes <- lapply(fix_fully_inline(math[bespoke]), xml2::xml_children)
+  new_nodes <- lapply(fix_fully_inline(imath), xml2::xml_children)
 
   # since we split up the nodes, we have to do this node by node
   for (i in seq(new_nodes)) {
-    add_node_siblings(math[bespoke][[i]], new_nodes[[i]])
+    add_node_siblings(math[bespoke][[i]], new_nodes[[i]], remove = TRUE)
   }
+
+  # protect math that is broken across lines
+  if (length(bmath[endless]) != length(bmath[headless])) {
+    stop("Uneven math elements")
+  }
+
+  # assign sequential tags to the pairs of inline math elements
+  tags <- seq(length(bmath[endless]))  
+  xml2::xml_set_attr(bmath[endless], "latex-pair", tags)
+  xml2::xml_set_attr(bmath[headless], "latex-pair", tags)
+  for (i in tags) {
+    fix_partial_inline(i, body, ns)
+  }
+  
   copy_xml(body)
+}
+
+fix_partial_inline <- function(tag, body, ns) {
+  # find everything between the tagged pair
+  to_find <- "md:text[@latex-pair='{tag}']"
+  math_lines <- find_block_math(body, ns, glue::glue(to_find), include = TRUE)
+  # make sure everything between the tagged pair is labeled as 'asis'
+  filling <- math_lines[is.na(xml2::xml_attr(math_lines, "latex-pair"))]
+  set_asis(filling)
+  filling <- xml2::xml_find_all(filling, ".//node()")
+  set_asis(filling)
+  # paste the lines together and create new nodes
+  n <- length(math_lines)
+  char <- as.character(math_lines)
+  char[[1]] <- sub("[$]", "$</text><text asis='true'>", char[[1]])
+  char[[n]] <- sub("[<]text ", "<text asis='true' ", char[[n]])
+  nodes <- paste(char, collapse = "")
+  nodes <- xml2::xml_children(set_default_space(nodes))
+  # nodes <- xml2::xml_children(fix_fully_inline(one_line))
+  # add the new nodes to the bottom of the existing math lines 
+  last_line <- math_lines[n]
+  to_remove <- math_lines[-n]
+  add_node_siblings(last_line, nodes, remove = TRUE)
+  # remove the duplicate lines
+  xml2::xml_remove(to_remove)
 }
 
 fix_fully_inline <- function(math) {
@@ -65,12 +126,17 @@ fix_fully_inline <- function(math) {
   # Find inline math that is complete and wrap it in text with asis
   # <text>this is $\LaTeX$ text</text>
   #   becomes
-  # <text>this is</text><text asis='true'> $\LaTeX$ </text><text>text</text>
+  # <text>this is </text><text asis='true'>$\LaTeX$</text><text> text</text>
   char <- gsub(
-    inline_dollars_regex("full"),
-    "</text><text asis='true'>\\1</text><text>", 
-    char
+    pattern = inline_dollars_regex("full"),
+    replacement = "</text><text asis='true'>\\1</text><text>", 
+    x = char,
+    perl = TRUE
   )
+  set_default_space(char)
+}
+
+set_default_space <- function(char) {
   new_nodes <- char_to_nodelist(char)
   n <- xml2::xml_find_all(new_nodes, ".//node()")
   # set space to default to avoid weird formatting (this may change)
