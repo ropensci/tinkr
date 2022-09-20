3e133e5f6e6b35befef6d8fad20df6d87f8ad549
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-09-20 10:51:15 +0200

    add test

--- tests/testthat/test-attr-nodes.R
+++ tests/testthat/test-attr-nodes.R
@@ -3,3 +3,17 @@ test_that("protect_curly() works", {
   curly <- yarn$new(pathcurly)
   expect_snapshot(cat(as.character(protect_curly(curly$body))))
 })
+
+test_that("a curly-protected yarn object can be written back to markdown", {
+  md_pathcurly <- system.file("extdata", "basic-curly.md", package = "tinkr")
+  rmd_pathcurly <- system.file("extdata", "basic-curly2.Rmd", package = "tinkr")
+  tmpdir <- withr::local_tempdir()
+  scarf1 <- withr::local_file(file.path(tmpdir, "yarn.md"))
+  scarf2 <- withr::local_file(file.path(tmpdir, "yarn.Rmd"))
+  y1 <- yarn$new(md_pathcurly)
+  y2 <- yarn$new(rmd_pathcurly)
+  y1$protect_curly()$write(scarf1)
+  y2$protect_curly()$write(scarf2)
+  expect_snapshot_file(scarf1)
+  expect_snapshot_file(scarf2)
+})
