3d17770bf507e2b0ceae407680f5dd04ee9cf5a6
Zhian N. Kamvar <zkamvar@gmail.com>
2022-09-20 12:00:16 -0700

    update test

--- tests/testthat/test-attr-nodes.R
+++ tests/testthat/test-attr-nodes.R
@@ -1,6 +1,6 @@
 test_that("protect_curly() works", {
   pathcurly <- system.file("extdata", "basic-curly.md", package = "tinkr")
-  curly <- yarn$new(pathcurly)
+  curly <- yarn$new(pathcurly, sourcepos = TRUE)
   expect_snapshot(cat(as.character(protect_curly(curly$body))))
 })
 
@@ -10,8 +10,8 @@ test_that("a curly-protected yarn object can be written back to markdown", {
   tmpdir <- withr::local_tempdir()
   scarf1 <- withr::local_file(file.path(tmpdir, "yarn.md"))
   scarf2 <- withr::local_file(file.path(tmpdir, "yarn.Rmd"))
-  y1 <- yarn$new(md_pathcurly)
-  y2 <- yarn$new(rmd_pathcurly)
+  y1 <- yarn$new(md_pathcurly, sourcepos = TRUE)
+  y2 <- yarn$new(rmd_pathcurly, sourcepos = TRUE)
   y1$protect_curly()$write(scarf1)
   y2$protect_curly()$write(scarf2)
   expect_snapshot_file(scarf1)
