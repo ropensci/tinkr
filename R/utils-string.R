dbe276736ae91319629de42a2136fc159a64a0f2
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-01-26 11:32:00 +0100

    make order more logical

--- R/utils-string.R
+++ R/utils-string.R
@@ -1,20 +1,20 @@
 # Replacements for stringr functions
 # to preserve the argument order + nice name
 
-str_remove_all <- function(string, pattern) {
-  gsub(pattern, "", string)
-}
-
-str_remove <- function(string, pattern) {
-  sub(pattern, "", string)
-}
-
 str_replace <- function(string, pattern, replacement) {
   sub(pattern, replacement, string)
 }
 
 str_replace_all <- function(string, pattern, replacement) {
   gsub(pattern, replacement, string)
+}
+
+str_remove <- function(string, pattern) {
+  str_replace(string, pattern, "")
+}
+
+str_remove_all <- function(string, pattern) {
+  str_replace_all(string, pattern, "")
 }
 
 str_detect <- function(string, pattern) {
