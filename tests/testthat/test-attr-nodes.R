37fc979930ac77769aa19755badba7ea69231b02
Zhian N. Kamvar <zkamvar@gmail.com>
2024-05-02 14:10:02 -0700

    update tests for curly

--- tests/testthat/test-attr-nodes.R
+++ tests/testthat/test-attr-nodes.R
@@ -1,8 +1,53 @@
 test_that("protect_curly() works", {
   pathcurly <- system.file("extdata", "basic-curly.md", package = "tinkr")
   curly <- yarn$new(pathcurly, sourcepos = TRUE)
-  expect_snapshot(cat(as.character(protect_curly(curly$body))))
+  protec <- protect_curly(curly$body)
+  expect_snapshot(cat(as.character(protec)))
 })
+
+
+test_that("protect_curly() can be reversed", {
+  pathcurly <- system.file("extdata", "basic-curly.md", package = "tinkr")
+  tmpfile <- withr::local_tempfile()
+  file.copy(pathcurly, tmpfile, overwrite = TRUE)
+  cat(c(
+    "",
+    "![image with long alt text](image.png){#image alt='this is",
+    "long alt text that should be all",
+    "included in the image'}", 
+    "",
+    "![image with short alt text](img.png){#img alt='short alt text'}",
+    ""
+    ),
+    sep = "\n",
+    file = tmpfile,
+    append = TRUE
+  )
+  curly <- yarn$new(tmpfile, sourcepos = TRUE)
+  curly$protect_curly()
+  orig <- copy_xml(curly$body)
+
+  expect_length(get_protected_nodes(curly$body), 3)
+  expect_length(xml2::xml_find_all(curly$body, ".//node()[@curly]"), 10)
+
+  # when split the protected nodes go away
+  splitsville <- split_protected_nodes(curly$body)
+  expect_snapshot(cat(as.character(splitsville)))
+  expect_length(get_protected_nodes(splitsville), 0)
+
+  joinsville <- join_split_nodes(splitsville)
+  expect_identical(joinsville, splitsville)
+  # joining restores these nodes
+  sprotec <- get_protected_nodes(splitsville)
+  expect_length(sprotec, 3)
+  expect_equal(
+    lapply(sprotec, get_protected_ranges),
+    lapply(get_protected_nodes(orig), get_protected_ranges)
+  )
+  expect_length(xml2::xml_find_all(splitsville, ".//node()[@curly]"), 10)
+  expect_snapshot(cat(as.character(joinsville)))
+})
+
 
 
 test_that("multiline alt text can be processed", {
